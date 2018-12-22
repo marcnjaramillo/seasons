86fb657ea9ca6b666f928f22ff98b2133242b556
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-12-21 21:10:36 -0800

    Begin to use state for latitude and error-handling

--- src/index.js
+++ src/index.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        );
 
-        return (
-            <div>Latitude:</div>
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({lat: position.coords.latitude});
+            },
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
         );
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;        
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading...</div>
+
     }
 }
 
